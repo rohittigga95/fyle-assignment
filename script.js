29b978af5db95e1c3083b3c55f2bff6fad4d338e
Rohit Tigga <rohit.yzx3135@gmail.com>
2024-07-13 17:22:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const photoChange = document.getElementById("change-photo");
+
+function changePhoto1() {
+    photoChange.src = "images/image.png";
+}
+
+function changePhoto2() {
+    photoChange.src = "images/image2.jpg";
+}
+
+function changePhoto3() {
+    photoChange.src = "images/image3.jpg";
+}
